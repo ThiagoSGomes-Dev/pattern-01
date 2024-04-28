576f334c90a5c2e023035ba738dcab25a4b1006d
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2024-04-28 05:18:16 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int i,j;
+        for(i = 1; i <= 8; i++) {
+            for(j = 8; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
